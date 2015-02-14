d5eac9c968212e2781eb391c91c30f076749035a
wschiro220 <cshanefelter@gmail.com>
2015-02-14 15:14:01 -0800

    R Programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## CDS 2-15-15 Creates list of 4 Function to be used in the cacheSolve function 
+##that gives results of inverse Matrix
 
-## Write a short comment describing this function
+## Assign values to golbal varibles x, m
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) m <<- solve
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+##CDS 2-15-15 Finds inverse of Matrix.  1st checks if inverse of matrix has 
+## been calculated if so returns result from varaible otherwise calculates inverse of Matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m    
 }
